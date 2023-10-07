6134a5c8632870edd9813071c317321b9cdd8675
Niteshmarwaha02 <123005559+Niteshmarwaha02@users.noreply.github.com>
2023-10-07 23:45:14 +0530

    Update Feedback.jsx

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -24,7 +24,7 @@ const Feedback = () => (
           <h4 className="font-bold sm:text-[32px] text-[26px]
           sm:leading-[40px] leading-[36px] text-white"
           >
-            Samantha
+            Nitesh Marwaha
           </h4>
           <p className="mt-[8px] flex-1 font-normal sm:text-[18px] text-[12px]
           sm:leading-[22px] leading-[16px] text-white"
