5100492679e062b1b11a7e5dadc854dfc171bd0a
Niteshmarwaha02 <123005559+Niteshmarwaha02@users.noreply.github.com>
2023-10-07 23:29:32 +0530

    Update Feedback.jsx

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -19,34 +19,10 @@ const Feedback = () => (
         className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05
       sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
       >
-<<<<<<< HEAD
         <div className="feedback-gradient" />
         <div className="mt-[40px]">
           <h4 className="font-bold sm:text-[32px] text-[26px]
           sm:leading-[40px] leading-[36px] text-white"
-=======
-        <motion.div
-          variants={fadeIn('right', 'tween', 0.2, 3)}
-          className='flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative '
-        >
-          <div 
-            className='feedback-gradient popUpAnimation'
-          />
-          <div>
-            <h4 className='font-bold sm:text-[32px] tex-[26px] sm:leading-[40px] leading-[36px] text-white'>Nitesh Marwaha</h4>
-            <p className='mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white'>Founder | Metaverse</p>
-          </div>
-          <p className='mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45px] leading-[39px] text-white'>"With the development of today's technology, metaverse is going to be a stepping stone into the world for VR and AR."</p>
-        </motion.div>
-        <motion.div
-          variants={fadeIn('left', 'tween', 0.2, 1)}
-          className='relative flex-1 flex justify-center items-center'
-        >
-          <img src="/planet-09.png" alt="planet-09" className='w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]'/>
-          <motion.div
-            variants={zoomIn(0.4, 1)}
-            className='lg:block hidden absolute -left-[10%] top-[3%]'
->>>>>>> 0c96ba60fc1f8edb4972d05ec3a48a260dbb44c6
           >
             Samantha
           </h4>
